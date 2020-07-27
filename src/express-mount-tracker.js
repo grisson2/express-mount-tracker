3ced033944cbf9f70e8e3a92d128cd58856d0324
Ivan Vaccari <ivan.vaccari@mitechsrl.it>
2020-07-27 16:42:04 +0200

    Added comments, fixed typos

--- src/express-mount-tracker.js
+++ src/express-mount-tracker.js
@@ -21,15 +21,21 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-*/'use strict';
+*/
+
+'use strict';
 
 module.exports = (router) => {
+    // add a property for holding values in this router
     router.mountedRoutes = [];
 
     const expressUse = router.use;
     router.use = function (...args) {
+
+        // this cover the case of '[app|router].get(path, middlewares)
         const _prepend = (typeof args[0] === 'string') ? args[0] : '';
 
+        // iterate over args to check for  mountedRoutes on subRouters
         args.forEach(m => {
             if (Array.isArray(m.mountedRoutes)) {
                 m.mountedRoutes.forEach(route => {
@@ -44,62 +50,89 @@ module.exports = (router) => {
             }
         });
 
+        // then just call the standard express.use function
         expressUse.call(router, ...args);
     };
 
+    /**
+     * Chcek for routes and sets them in a [app|router] property
+     * @param {*} args 
+     * @param {*} method 
+     */
     const track = (args, method) => {
+
+        
         if (typeof args[0] === 'string') {
+            // this cover the case of '[app|router].something(path, middlewares)
             router.mountedRoutes.push({ method: method, path: args[0] });
+
         } else if (typeof args[0] === 'object' && typeof args[0].path === 'string' && typeof args[0].id === 'string') {
+            // this cover the custom case '[app|router].get({id, path, data}, middlewares)
             router.mountedRoutes.push({ method: method, path: args[0].path, id: args[0].id, data: args[0].data });
             args.splice(0, 1, args[0].path);
         }
     };
 
+    /**
+     * Redefinition of get
+     */
     const expressGet = router.get;
     router.get = function (...args) {
         // apply tracking
         track(args, 'get');
 
-        // ten just like the standard espress router.get
+        // then just call the standard espress router.get
         expressGet.call(router, ...args);
     };
 
+    /**
+     * Redefinition of post
+     */
     const expressPost = router.post;
     router.post = function (...args) {
         // apply tracking
         track(args, 'post');
 
-        // ten just like the standard espress router.post
+        // then just call the standard espress router.post
         expressPost.call(router, ...args);
     };
 
+    /**
+     * Redefinition of delete
+     */
     const expressDelete = router.post;
     router.delete = function (...args) {
         // apply tracking
         track(args, 'delete');
 
-        // ten just like the standard espress router.delete
+        // then just call the standard espress router.delete
         expressDelete.call(router, ...args);
     };
 
+    /**
+     * Redefinition of put
+     */
     const expressPut = router.post;
     router.put = function (...args) {
         // apply tracking
         track(args, 'put');
 
-        // ten just like the standard espress router.put
+        // then just call the standard espress router.put
         expressPut.call(router, ...args);
     };
 
+    /**
+     * Redefinition of patch
+     */
     const expressPatch = router.post;
     router.patch = function (...args) {
         // apply tracking
         track(args, 'patch');
 
-        // ten just like the standard espress router.patch
+        // then just call the standard espress router.patch
         expressPatch.call(router, ...args);
     };
 
+    // TODO: redefine [app|router].route
     return router;
 };
